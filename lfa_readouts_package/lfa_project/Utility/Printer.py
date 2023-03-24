edcebf3b74922a9e826f0f52382c1a038afbab24
morerdejlig <76737356+morerdejlig@users.noreply.github.com>
2023-03-24 13:29:21 +0100

    refactoring

--- lfa_readouts_package/lfa_project/Utility/Printer.py
+++ lfa_readouts_package/lfa_project/Utility/Printer.py
@@ -2,10 +2,10 @@ import datetime
 import cv2 as cv
 import os
 
-class Printing():
+class Printer():
 
     def __init__(self):
-        self.folderPath = None
+        self.folder_path = None
         self.create_folder()
 
     def create_folder(self):
@@ -13,20 +13,20 @@ class Printing():
         
         folder_name = now.strftime('%Y-%m-%d_%H-%M-%S')
         
-        folderPath = os.path.join(os.getcwd(), "lfa_readouts_package", "lfa_project", "Results", folder_name)
+        folder_path = os.path.join(os.getcwd(), "lfa_readouts_package", "lfa_project", "Results", folder_name)
         
-        os.makedirs(folderPath, exist_ok=True)
+        os.makedirs(folder_path, exist_ok=True)
         
-        self.folderPath = folderPath
+        self.folder_path = folder_path
 
     def write_image(self, image, name, contours = []):
         
         if len(contours):
             cv.drawContours(image, contours, -1, (0, 255, 0), 3)
 
-        pictureName = name + ".png"
+        picture_name = name + ".png"
 
-        img_path = os.path.join(self.folderPath, pictureName)
+        img_path = os.path.join(self.folder_path, picture_name)
 
         cv.imwrite(img_path, image)
         
@@ -37,7 +37,7 @@ class Printing():
 
         txt_name = now.strftime("%Y%m%d%H%M%S%f") + ".txt"
 
-        txt_path = os.path.join(self.folderPath, txt_name)
+        txt_path = os.path.join(self.folder_path, txt_name)
 
         with open(txt_path, "w") as f:
             f.write(message)
